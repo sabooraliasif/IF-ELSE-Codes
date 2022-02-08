0ca89a9e2b33554d01ee9ef826abd4ed34226ef7
SABOOR ALI ASIF <98776073+sabooraliasif@users.noreply.github.com>
2022-02-08 22:33:57 +0500

    Add files via upload

---
+++ percentage code.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main(){
+	float A,B;
+	cout<<"Enter prcentage of A and B?"<<endl;
+	cin>>A;
+	cin>>B;44666
+	if (A>=55 && B>=45)
+	{
+		cout<<"Student is passed."<<endl;
+	}
+	else if  (A>=45 && A<55 && B>=55)
+	{
+		cout<<"Student is passed."<<endl;
+	}
+	else if (A>=45 && B<45)
+	{
+		cout<<"Student is allowed to reappear in an exam."<<endl;
+	}
+	else
+	{
+		cout<<"Student is failed."<<endl;
+	}
+	return 0;
+	
+	
+}
